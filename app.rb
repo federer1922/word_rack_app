c90ffd56f22cb77669f1ceffa20991eb384909aa
Arkadiusz Karpinski <arekkarpinski2@gmail.com>
2021-04-18 21:23:08 +0200

    initialize method

--- app.rb
+++ app.rb
@@ -3,16 +3,19 @@ require "http"
 require "pry"
 
 class App
-  def call(env)
-    response = HTTP.get("https://raw.githubusercontent.com/cmusphinx/cmudict/master/cmudict.dict?fbclid=IwAR3ke7nHSguLnRmynPmnJIlEQQqAGhXxZWLghswCpcX6mabsnow3WbUWqp0")
-    status  = 200
-    headers = { "Content-Type" => "text/html" }
-    result = {}
-    response.body.to_s.split("\n").each do |line|
+  def initialize
+    @response = HTTP.get("https://raw.githubusercontent.com/cmusphinx/cmudict/master/cmudict.dict?fbclid=IwAR3ke7nHSguLnRmynPmnJIlEQQqAGhXxZWLghswCpcX6mabsnow3WbUWqp0")
+    @result = {}
+    @response.body.to_s.split("\n").each do |line|
       word, pronunciation = line.split(" ", 2)
-      result[word] = pronunciation
+      @result[word] = pronunciation
     end
-    body = [result.keys.first.to_s]
+  end
+  
+  def call(env)
+    status  = 200
+    headers = { "Content-Type" => "text/html" } 
+    body = [@result.to_json]
     [status, headers, body]
   end
 end
